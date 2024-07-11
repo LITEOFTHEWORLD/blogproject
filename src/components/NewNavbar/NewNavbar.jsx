4f4a54270501378000aa6946652a9ee498bc6460
liteoftheworld <kosuruth100@gmail.com>
2024-07-11 15:56:09 +0100

    finished i guess

--- src/components/NewNavbar/NewNavbar.jsx
+++ src/components/NewNavbar/NewNavbar.jsx
@@ -1,3 +1,55 @@
+// "use client";
+// import React, { useState } from "react";
+// import { FaBars, FaTimes } from "react-icons/fa";
+// import Link from "next/link";
+// import styles from "./NewNavbar.module.css";
+// import ThemeToggle from "../themeToggle/ThemeToggle";
+
+// const NewNavbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.social}>
+//         <div>
+//           <p className={styles.logo_text}>
+//             Tech<span className={styles.bold_text}>BlackGirl</span>
+//           </p>
+//         </div>
+//       </div>
+
+//       <div className={`${styles.links} ${isOpen ? styles.linksOpen : ""}`}>
+//         <Link href="/" className={styles.link}>
+//           Home
+//         </Link>
+//         <Link href="/blog" className={styles.link}>
+//           Blog
+//         </Link>
+//         <Link href="/authur" className={styles.link}>
+//           Author
+//         </Link>
+//         <Link href="/contact" className={styles.link}>
+//           Contact
+//         </Link>
+//       </div>
+//       <div className={styles.topNav}>
+//         <input type="text" placeholder="Search.." />
+//       </div>
+//       <div>
+//         <ThemeToggle />
+//       </div>
+//       <div className={styles.hamburger} onClick={toggleMenu}>
+//         {isOpen ? <FaTimes /> : <FaBars />}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NewNavbar;
+
 "use client";
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
@@ -20,6 +72,9 @@ const NewNavbar = () => {
             Tech<span className={styles.bold_text}>BlackGirl</span>
           </p>
         </div>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
       </div>
 
       <div className={`${styles.links} ${isOpen ? styles.linksOpen : ""}`}>
@@ -40,13 +95,8 @@ const NewNavbar = () => {
       <div className={styles.topNav}>
         <input type="text" placeholder="Search.." />
       </div>
-
       <div>
         <ThemeToggle />
-      </div>
-
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        {isOpen ? <FaTimes /> : <FaBars />}
       </div>
     </div>
   );
