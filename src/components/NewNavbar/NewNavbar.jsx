467d9e29eb2e26021a635dc93d83af3409d97fdc
liteoftheworld <kosuruth100@gmail.com>
2024-07-11 14:30:03 +0100

    so i just added the hamburger menu

--- src/components/NewNavbar/NewNavbar.jsx
+++ src/components/NewNavbar/NewNavbar.jsx
@@ -1,44 +1,54 @@
-import React from "react";
-import styles from "./NewNavbar.module.css";
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link";
-
+import styles from "./NewNavbar.module.css";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const NewNavbar = () => {
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.social}>
-          <div>
-            <p className={styles.logo_text}>
-              Tech<span className={styles.bold_text}>BlackGirl</span>
-            </p>
-          </div>
-        </div>
-        <div className={styles.links}>
-          <Link href="/ " className={styles.link}>
-            Home
-          </Link>
-          <Link href="/blog" className={styles.link}>
-            Blog
-          </Link>
+  const [isOpen, setIsOpen] = useState(false);
 
-          <Link href="/authur" className={styles.link}>
-            authur
-          </Link>
-          <Link href="/contact" className={styles.link}>
-            contact
-          </Link>
-        </div>
-        <div class={styles.topNav}>
-          <input type="text" placeholder="Search.." />
-        </div>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
         <div>
-          <ThemeToggle />
+          <p className={styles.logo_text}>
+            Tech<span className={styles.bold_text}>BlackGirl</span>
+          </p>
         </div>
       </div>
-    </>
+
+      <div className={`${styles.links} ${isOpen ? styles.linksOpen : ""}`}>
+        <Link href="/" className={styles.link}>
+          Home
+        </Link>
+        <Link href="/blog" className={styles.link}>
+          Blog
+        </Link>
+        <Link href="/authur" className={styles.link}>
+          Author
+        </Link>
+        <Link href="/contact" className={styles.link}>
+          Contact
+        </Link>
+      </div>
+
+      <div className={styles.topNav}>
+        <input type="text" placeholder="Search.." />
+      </div>
+
+      <div>
+        <ThemeToggle />
+      </div>
+
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+    </div>
   );
 };
 
